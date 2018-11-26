16b82dffcd7689418ea8e0399d696b41bb350bce
lvqiya <1210806488@qq.com>
2018-11-26 15:35:25 +0800

    登录页面结构搭建

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    
+    <router-view></router-view>
   </div>
 </template>
 
@@ -18,6 +18,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
+}
+#app {
+  height: 100%;
 }
 </style>
---
+++ src/components/login/login.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="login-wrap">
+        <el-form :label-position="top" label-width="80px" :model="formdata" class="login-form">
+            <h3>用户登录</h3>
+            <el-form-item label="用户名">
+                <el-input v-model="formdata.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formdata.password"></el-input>
+            </el-form-item>
+            <el-button type="primary" round class="login-btn">主要按钮</el-button>
+        </el-form>
+    </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formdata: {
+        username: "",
+        password: ""
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login-wrap {
+  background-color: #324152;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.login-wrap .login-form {
+  background-color: #fff;
+  width: 400px;
+  padding: 30px;
+  border-radius: 5px;
+}
+.login-wrap .login-form h3 {
+    margin: 0;
+}
+.login-wrap .login-form .login-button {
+  width: 100%;
+}
+</style>
