a6f0cb6c29e5000c0bad3c45b4f0e8c28ca107c2
lvqiya <1210806488@qq.com>
2018-11-28 18:09:09 +0800

    拦截器获取token完成

--- src/assets/plugins/http.js
+++ src/assets/plugins/http.js
@@ -1,8 +1,21 @@
 import axios from 'axios'
 import Vue from 'vue';
 const MyHttpServer = {}
-MyHttpServer.install = (vue)=>{
-    axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
-    Vue.prototype.$http = axios
+MyHttpServer.install = (vue) => {
+  axios.defaults.baseURL = 'http://localhost:8888/api/private/v1/'
+  axios.interceptors.request.use(function (config) {
+    // console.log(config);
+    if (config.url !== 'login') {
+      const token = localStorage.getItem('token')
+      config.headers.Authorization = token
+    }
+    // 在发送请求之前做些什么
+
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+  Vue.prototype.$http = axios
 }
 export default MyHttpServer
